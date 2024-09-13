50dec2473d714fa6c6c3c6848c2e3a1415d1b444
DCR911 <dylan.carreaurodriguez@dawsoncollege.qc.ca>
2024-09-13 23:05:49 +0000

    I was bored so I added round ups

--- volume.py
+++ volume.py
@@ -1,4 +1,8 @@
 import math
 r = float(input("Please imput the radius of the desired sphere : "))
 V = (r**3)* math.pi * 4/3
-print("The volume of your desired sphere is " + str(V))
+x = float(input("How many numbers do you want after the point if you do not want any round up then input '0' : "))
+if(x == 0):
+  print("The volume of your desired sphere is " + str(V))
+else:
+  print("The volume of your desired sphere is " + str(round(V, int(x))))
